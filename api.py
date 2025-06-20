bec6d491e5f64b0825dc193ea75ac053aa95b1d6
hrishikesh-bade-dure <hrishikesh.bade@duretechnologies.com>
2025-06-20 14:39:05 +0530

    added age parameter to the user

--- api.py
+++ api.py
@@ -13,9 +13,10 @@ class UserModal(db.Model): # Define User model
     id = db.Column(db.Integer, primary_key=True) # Primary key
     name = db.Column(db.String(80), unique=True, nullable=False) # Name field
     email = db.Column(db.String(120), unique=True, nullable=False) # Email field
+    age = db.Column(db.Integer, nullable=False) # Age field
 
     def __repr__(self): # String representation
-        return f"User (name = {self.name}, email = {self.email})" # Define how the User model is represented
+        return f"User (name = {self.name}, email = {self.email}, age = {self.age})" # Define how the User model is represented
 
 user_args = reqparse.RequestParser() # Initialize request parser for user input
 
@@ -23,10 +24,13 @@ user_args.add_argument('name', type=str, help='Name cannot be blank', required=T
 
 user_args.add_argument('email', type=str, help='Email cannot be blank', required=True) # Add email argument to parser
 
+user_args.add_argument('age', type=int, help='Age cannot be blank', required=True) # Add age argument to parser
+
 userFields = {
     'id': fields.Integer, # Define fields for marshalling
     'name': fields.String,
-    'email': fields.String
+    'email': fields.String,
+    'age': fields.Integer,
 }
 
 class Users(Resource): # Define Users resource for API
@@ -38,7 +42,7 @@ class Users(Resource): # Define Users resource for API
     @marshal_with(userFields) # Use marshalling to format output
     def post(self):
         args = user_args.parse_args()
-        user = UserModal(name=args['name'], email=args['email'])
+        user = UserModal(name=args['name'], email=args['email'], age=args['age'])
         db.session.add(user)
         db.session.commit()
         users = UserModal.query.all()
@@ -57,8 +61,9 @@ class User(Resource): # Define User resource for API
         user = UserModal.query.filter_by(id=id).first() # Get user
         if not user:
             abort(404, message="User not found") # User not found
-        user.name = args['name'] # Update name and email
+        user.name = args['name'] # Update name and email and age
         user.email = args['email']
+        user.age = args['age']
         db.session.commit()
         return user
     @marshal_with(userFields)
@@ -80,4 +85,4 @@ def home(): # Home route function
     return '<h1>Welcome to the Demo Project 2 API!</h1>'    # Return welcome message 
 
 if __name__ == '__main__':  # Check if the script is run directly
-    app.run(debug=True) # Run the Flask app in debug mode
+     app.run(debug=True) # Run the Flask app in debug mode
